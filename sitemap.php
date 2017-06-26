83a51dc40772c6367a8e36e6619eb9dd5a76fd81
Lee Garner <lee@leegarner.com>
2017-06-26 11:41:38 -0700

    Bump version to 2.0.1

--- sitemap.php
+++ sitemap.php
@@ -52,7 +52,7 @@ $_TABLES['smap_maps'] = $_SMAP_table_prefix . 'smap_maps';
 // Plugin info
 $_SMAP_CONF['pi_name']          = 'sitemap';
 $_SMAP_CONF['pi_display_name']  = 'SiteMap';
-$_SMAP_CONF['pi_version']       = '2.0.0';
+$_SMAP_CONF['pi_version']       = '2.0.1';
 $_SMAP_CONF['gl_version']       = '1.6.0';
 $_SMAP_CONF['pi_url']           = 'https://www.glfusion.org/';
 
